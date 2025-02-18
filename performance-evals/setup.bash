a3885ee7fb3b5f0b7b04e5783c790ea8779ca73d
HerschaLo <stephen.dt.le@gmail.com>
2025-02-18 16:52:03 -0500

    Adding linux ollama binary for supercomputer

---
+++ performance-evals/setup.bash
@@ -0,0 +1,15 @@
+pip install -r requirements.txt
+ollama serve &
+
+ollama_models=(
+    llama3.2:3b
+    deepseek-r1:1.5b
+    qwen:1.8b
+    gemma2:2b
+    phi3:3.8b
+    mistral
+)
+
+for item in "${items[@]}"; do
+    ollama pull $item
+done
