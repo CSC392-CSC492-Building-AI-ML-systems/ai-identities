061d4349f0e3c0256453f79578b4fe27bf7dea36
Stephen Le <stephen.dt.le@gmail.com>
2025-05-30 22:10:11 -0400

    Adjust api key settings

--- response_classifier/get_model_data.bash
+++ response_classifier/get_model_data.bash
@@ -3,7 +3,7 @@
 . set_api_key.bash
 if ! python3 get_model_data.py --url  https://api.deepinfra.com/v1/openai   \
     --model YOUR_MODEL_HERE \
-    --api_key $DEEPINFRA_API_KEY \
+    --api_key $API_KEY \
     --temperature 0.0; 
     then
     echo "Python script exited with an error. Terminating early." | tee /dev/tty
